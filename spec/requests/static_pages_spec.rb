0ec224fc10b6bbfabcaca0022cd4a3090678cd73
Seth Wainer <sethwainer@gmail.com>
2013-01-09 12:48:25 -0500

    Finish Static Pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,51 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  
+  describe "home page" do
+    it "should have the content sample app" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+
+    it "should have the right title" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => " | Home")
+    end
+
+  end
+
+    describe "help page" do
+
+    it "should have the content help" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the right title" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => " | Help")
+    end
+  end
+
+    describe "about page" do
+
+    it "should have the content about us" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+  
+    it "should have the right title" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => " | About")
+    end
+  end
+
+ 
+end
