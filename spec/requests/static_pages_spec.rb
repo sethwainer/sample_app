b2a5ff04223badadef6d95b585f29dda808a5220
Seth Wainer <sethwainer@gmail.com>
2013-01-09 17:02:19 -0500

    Learning Some Ruby

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -9,10 +9,16 @@ describe "StaticPages" do
       page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have the right title" do
+	it "should have the base title" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       visit '/static_pages/home'
-      page.should have_selector('title', :text => " | Home")
+      page.should have_selector('title', :text => "Ruby on Rails Sample App")
+    end
+
+    it "should not have the custom title" do
+      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => " | Home")
     end
 
   end
